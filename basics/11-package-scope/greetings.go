018f613ad128076fa3c18d4d7128945ab6b3c180
Valerii Oleksiienko <akerman8048@gmail.com>
2026-07-23 13:26:54 +0000

    learn package scope

---
+++ basics/11-package-scope/greetings.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+var points = []int{20,90,100,35,60}
+
+func sayHello(n string) {
+fmt.Println("hello", n)
+}
+
+func showScope() {
+	fmt.Println("you score this many points", score)
+}
---
+++ basics/11-package-scope/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+var score = 99.6
+
+func main() {
+	sayHello("Levi")
+
+	for _, v := range points {
+		fmt.Println(v)
+	}
+
+	showScope()
+}
